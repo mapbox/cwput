fa12a23cc608bedfe0e008ded54e649ff2767753
Ian Ward <ianshward@gmail.com>
2014-01-30 10:17:46 -0500

    Run ./install.bash from anywhere!

--- install.bash
+++ install.bash
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-cp ./bin/cwput.bash /usr/bin
-cp ./etc/cwput.conf /etc/init
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cp $dir/bin/cwput.bash /usr/bin
+cp $dir/etc/cwput.conf /etc/init
 mkdir /etc/cwput
-cp -r ./etc/checks /etc/cwput
+cp -r $dir/etc/checks /etc/cwput
 start cwput
