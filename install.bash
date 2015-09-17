35dc2359b2a3a9c483dc2eb19c6a2609e8bce21e
Z <zmully@925labs.com>
2015-09-17 18:03:56 -0400

    fixed launchd environment variable bugs and added uninstall script

--- install.bash
+++ install.bash
@@ -10,21 +10,25 @@ dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cp $dir/bin/cwput.bash /usr/bin
 cp $dir/bin/cwput.start /usr/bin
 
+
 mkdir -p /etc/cwput
-cp -r $dir/etc/checks /etc/cwput
+if [[ "$OSTYPE" =~ darwin ]]; then
+    cp -r $dir/etc/darwin/checks /etc/cwput
+else
+    cp -r $dir/etc/linux/checks /etc/cwput
+fi
 
 init=`lsof -w -a -p 1 -d txt`
 
 if [[ $init =~ systemd ]]; then
-    echo "systemd found"
     cp $dir/bin/cwput.start /usr/bin/cwput.start
     cp $dir/etc/cwput.service /etc/systemd/system
     systemctl start cwput
 elif [[ $init =~ launchd ]]; then
-    echo "launchd found"
-
+    cp $dir/bin/cwput.start /usr/bin/cwput.start
+    cp $dir/etc/com.mapbox.cwput.plist /Library/LaunchDaemons
+    launchctl load /Library/LaunchDaemons/com.mapbox.cwput.plist
 else
-    echo "upstart guessed"
     cp $dir/etc/cwput.conf /etc/init
     start cwput
 fi
---
+++ uninstall.bash
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+if  [[ $(id -u) -ne 0 ]]; then
+        echo "not root, please su up and rerun script"
+        exit 1
+fi
+
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+init=`lsof -w -a -p 1 -d txt`
+
+if [[ $init =~ systemd ]]; then
+    systemctl stop cwput
+    rm /etc/systemd/system
+elif [[ $init =~ launchd ]]; then
+    launchctl unload /Library/LaunchDaemons/com.mapbox.cwput.plist
+    rm /Library/LaunchDaemons/com.mapbox.cwput.plist
+    rm /tmp/cwput.out
+    rm /tmp/cwput.err
+else
+    stop cwput
+    rm /etc/init/cwput.conf
+fi
+
+rm /usr/bin/cwput.bash
+rm /usr/bin/cwput.start
+rm -Rf /etc/cwput
